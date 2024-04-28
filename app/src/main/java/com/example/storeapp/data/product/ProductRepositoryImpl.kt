3c77e2a9a2f3191e207438a01a12a92434df43ae
Roberto <rovermore@gmail.com>
2024-04-28 12:22:12 +0200

    fixing mappers

--- app/src/main/java/com/example/storeapp/data/product/ProductRepositoryImpl.kt
+++ app/src/main/java/com/example/storeapp/data/product/ProductRepositoryImpl.kt
@@ -1,13 +1,10 @@
 package com.example.storeapp.data.product
 
 import com.example.storeapp.data.base.APIErrorMapper
-import com.example.storeapp.data.cart.local.ProductLocalDatasource
 import com.example.storeapp.data.product.model.ProductResponseMapper
 import com.example.storeapp.data.product.network.ProductNetworkDatasource
 import com.example.storeapp.domain.base.Error
-import com.example.storeapp.domain.base.Failure
 import com.example.storeapp.domain.base.Result
-import com.example.storeapp.domain.base.Success
 import com.example.storeapp.domain.base.map
 import com.example.storeapp.domain.base.mapFailure
 import com.example.storeapp.domain.product.model.ProductDTO
@@ -23,9 +20,9 @@ class ProductRepositoryImpl @Inject constructor(
     override fun getProductCatalog(): Result<List<ProductDTO>, Error> {
         return productNetworkDatasource.getProductsCatalog()
             .map {
-                return Success(productResponseMapper.mapList(it))
+                productResponseMapper.mapList(it)
             }.mapFailure {
-                return Failure(apiErrorMapper.map(it))
+                apiErrorMapper.map(it)
             }
     }
 
